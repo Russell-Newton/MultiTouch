8d73c1f3039bcc6443d64bd2e2b38477722b402d
ocean-ift <karimiftekherul689@gmail.com>
2022-09-29 23:55:26 -0400

    stash

--- gesturelibrary/src/tap.c
+++ gesturelibrary/src/tap.c
@@ -3,7 +3,8 @@
 tap_data_t data[MAX_TOUCHES];
 
 void recognize_tap(touch_event_t* event) {
-    // TODO: update state based on event
+    // this is gonna update the state as possible as long as the location are within a certain close knit range
+    
 }
 
 tap_data_t* get_tap_data() {

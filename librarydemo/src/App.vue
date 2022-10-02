9a83b9abcffa2e44bf41e578360e4fe55be5d038
deborahsrcho <deborahsrcho@gmail.com>
2022-10-02 19:38:41 -0400

    new image added
    Gesture image added to page

--- librarydemo/src/App.vue
+++ librarydemo/src/App.vue
@@ -5,6 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
+  <img alt="Vue logo" class="logo" src="./assets/gesture.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="GT Team 2135 x L3Harris Gesture Library" />

c843d1f8f94fee2c6f4c448dea4a0e971621fa36
Russell Newton <russell.newton01@gmail.com>
2022-10-09 15:14:13 -0400

    Revert "added unit tests to test pre-commit"
    This reverts commit 5d99300de97529162bb0b08fee92001d7aaef27a.

--- librarydemo/c_bindings/src/test.c
+++ librarydemo/c_bindings/src/test.c
@@ -3,7 +3,11 @@
 #include <emscripten.h>
 
 EMSCRIPTEN_KEEPALIVE
-void say_hi() { printf("Hello, World!"); }
+void say_hi() {
+    printf("Hello, World!");
+}
 
 EMSCRIPTEN_KEEPALIVE
-float square(float x) { return x * x; }
+float square(float x) {
+    return x * x;
+}
--- librarydemo/vite.config.js
+++ librarydemo/vite.config.js
@@ -1,11 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {fileURLToPath, URL} from 'node:url'
-import {defineConfig} from 'vite'
 import wasm from "vite-plugin-wasm"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '',
-  plugins : [ vue(), wasm() ],
-  resolve : {alias : {'@' : fileURLToPath(new URL('./src', import.meta.url))}}
+  base: '',
+  plugins: [
+    vue(),
+    wasm()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })

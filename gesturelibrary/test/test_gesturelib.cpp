4b419835ee12e030ab5b778c28096d899a4186e7
Iftekherul Karim <karim@DESKTOP-56DHO31.localdomain>
2022-10-09 21:24:32 -0400

    added unit testing for single finger drag

--- gesturelibrary/test/test_gesturelib.cpp
+++ gesturelibrary/test/test_gesturelib.cpp
@@ -32,6 +32,26 @@ TEST(GestureLibraryGetDataTest, TestGestureLibGetters) {
 
     ASSERT_TRUE(get_sFingerTap() != NULL);
     ASSERT_TRUE(get_sFingerSwipe() != NULL);
-    ASSERT_TRUE(get_sFingerHold() != NULL);
     ASSERT_TRUE(get_sFingerDTap() != NULL);
+}
+
+TEST(GestureLibraryHoldTest, TestGestureHoldTester) {
+    init_gesturelib();
+
+    touch_event_t event1;
+    event1.position_x = 100;
+    event1.position_y = 200;
+    event1.timestamp  = 42;
+
+    touch_event_t event2;
+    event2.position_x = 100;
+    event2.position_y = 200;
+    event2.timestamp  = 47;
+
+    gesture_event_t gestures[1];
+
+    process_touch_event(&event1, gestures, 4);
+    process_touch_event(&event2, gestures, 4);
+
+    EXPECT_EQ(get_sFingerHold()[0].state, possible);
 }

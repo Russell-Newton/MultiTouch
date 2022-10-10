f066dd26fe3f0858fc88530d05acec46e6e2da3d
jrdike <jackson.r.dike@gmail.com>
2022-10-09 23:33:19 -0400

    #64 - Preliminary Implementation of SingleFingerTap

--- gesturelibrary/src/single/singleFingerTap.c
+++ gesturelibrary/src/single/singleFingerTap.c
@@ -8,6 +8,10 @@ void recognize_single_tap(touch_event_t* event) {
     // TODO: update state based on event (complete in sprint 3)
     // NOTE: if the timestamp of the new event is too far from the timestamp of the last one, this can no longer be
     // possible
+
+    if (!(event->down)) {
+        sFingerTap_d[0].state = (sFingerTap_d[0].state == possible) ? complete : failed;
+    }
 }
 
 sFingerTap_t* get_sFingerTap() {

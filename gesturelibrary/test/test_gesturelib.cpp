e4511774f809643ada0407b4a966a062424e9ef7
Iftekherul Karim <karim@DESKTOP-56DHO31.localdomain>
2022-10-09 21:59:52 -0400

    added unit testing for sFinger double tap

--- gesturelibrary/test/test_gesturelib.cpp
+++ gesturelibrary/test/test_gesturelib.cpp
@@ -55,3 +55,24 @@ TEST(GestureLibraryHoldTest, TestGestureHoldTester) {
 
     EXPECT_EQ(get_sFingerHold()[0].state, possible);
 }
+
+TEST(GestureLibrarySFingerDoubleTest, TestGestureSingleDouble) {
+    init_gesturelib();
+
+    touch_event_t event1;
+    event1.position_x = 100;
+    event1.position_y = 200;
+    event1.timestamp  = 42;
+
+    touch_event_t event2;
+    event2.position_x = 100;
+    event2.position_y = 200;
+    event2.timestamp  = 44;
+
+    gesture_event_t gestures[1];
+
+    process_touch_event(&event1, gestures, 4);
+    process_touch_event(&event2, gestures, 4);
+
+    EXPECT_EQ(get_sFingerDTap()[0].state, possible);
+}

7b52611d0b0e35f27bb34f1e1b21f692ef80351c
jrdike <jackson.r.dike@gmail.com>
2022-10-09 23:34:31 -0400

    #64 - Preliminary 1Fingertap

--- gesturelibrary/src/single/singleFingerTap.c
+++ gesturelibrary/src/single/singleFingerTap.c
@@ -4,6 +4,8 @@
 
 sFingerTap_t sFingerTap_d[MAX_TOUCHES];
 
+static touch_event_t* prev_event;
+
 void recognize_single_tap(touch_event_t* event) {
     // TODO: update state based on event (complete in sprint 3)
     // NOTE: if the timestamp of the new event is too far from the timestamp of the last one, this can no longer be

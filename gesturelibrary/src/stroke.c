6567dd1a695f0daf08ba6ef61e22d0fadd2a774c
Wenjun Wang <wenjun.wang.3024@gmail.com>
2022-10-26 12:58:41 -0400

    corrected stroke velocity calculation

--- gesturelibrary/src/stroke.c
+++ gesturelibrary/src/stroke.c
@@ -73,13 +73,13 @@ static void update_velocity(stroke_t* stroke, float vx, float vy) {
     } else {
         stroke->vx *= STROKE_CACHE_SIZE;
         stroke->vy *= STROKE_CACHE_SIZE;
+        stroke->cache_last = (stroke->cache_last + 1) % STROKE_CACHE_SIZE;
         stroke->vx -= stroke->cache_vx[stroke->cache_last];
         stroke->vy -= stroke->cache_vy[stroke->cache_last];
-        stroke->cache_last                   = (stroke->cache_last + 1) % STROKE_CACHE_SIZE;
         stroke->cache_vx[stroke->cache_last] = vx;
         stroke->cache_vy[stroke->cache_last] = vy;
-        stroke->vx += stroke->cache_vx[stroke->cache_last];
-        stroke->vy += stroke->cache_vy[stroke->cache_last];
+        stroke->vx += vx;
+        stroke->vy += vy;
         stroke->vx /= STROKE_CACHE_SIZE;
         stroke->vy /= STROKE_CACHE_SIZE;
     }

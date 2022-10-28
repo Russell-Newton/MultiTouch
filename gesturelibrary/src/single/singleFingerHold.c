02b08899b0b9f7ad48d4428d55c9251ff6b1cfce
Iftekherul Karim <karim@DESKTOP-56DHO31.localdomain>
2022-10-27 21:38:35 -0400

    pulled main

--- gesturelibrary/src/single/singleFingerHold.c
+++ gesturelibrary/src/single/singleFingerHold.c
@@ -2,25 +2,95 @@
 
 // this class takes care of the single finger cases
 
-sFingerHold_t sFingerHold_d[MAX_TOUCHES];
+sFingerHold_t* sFingerHold_d[MAX_TOUCHES];
+static touch_event_t* start;
+static touch_event_t* prev = 0;
+static state_t updated_state;
+static gesture_event_t* hold_gesture;
 
-static touch_event_t* prev_event;
-
-static touch_event_t* prev_events[HOLD_LENGTH];
+int size;
+int d_index;
 
 gesture_event_t* recognize_single_hold(touch_event_t* event) {
-    // TODO: look at the last x timestamps/milliseconds of touch data, verifying they are in the same location there are
-    // n continuos touch_events that streamed in during that time (say we recieve data at 100 ms intervals and our hold
-    // needs to be a second long) if last was a drag (drag's state is possible) this is default false if we didn't see a
-    // null at the end of it
+    switch (event->event_type) {
+    case TOUCH_EVENT_DOWN:
+        process_hold_down(event);
+        clear_data();
+        break;
+    case TOUCH_EVENT_MOVE:
+        process_hold_move(event);
+        break;
+    case TOUCH_EVENT_UP:
+        process_hold_up(event);
+        break;
+    }
 
-    // TODO: The following section has been commented since it causes a segfault.
-    // if (event->t - prev_event->t > 10) {
-    //     // clear prev events
-    //     sFingerHold_d[0].state = RECOGNIZER_STATE_POSSIBLE;
-    // }
+    add_to_data(create_touch_data(updated_state, event));
+    // sFingerHold_d[0] = create_touch_data(updated_state, event);
 
-    return 0;
+    gesture_event_t hold;
+    hold.type        = GESTURE_TYPE_HOLD;
+    hold.num_touches = 1;
+    hold.get_data    = (void* (*)(void))get_sFingerHold;
+    hold_gesture     = &hold;
+    return hold_gesture;
+}
+
+void add_to_data(sFingerHold_t* data) {
+    // add data circularly
+
+    if (d_index == MAX_TOUCHES) {
+        d_index = 0;
+    }
+
+    sFingerHold_d[size] = data;
+    if (size != MAX_TOUCHES)
+        size++;
+    d_index++;
+}
+
+void clear_data() {
+    for (int i = 0; i < MAX_TOUCHES; i++) {
+        sFingerHold_d[i] = 0;
+    }
+}
+
+sFingerHold_t* create_touch_data(state_t state, touch_event_t* event) {
+    sFingerHold_t sFingerHold_data;
+    sFingerHold_data.state     = state;
+    sFingerHold_data.last_x    = event->position_x;
+    sFingerHold_data.last_y    = event->position_y;
+    sFingerHold_data.last_time = event->timestamp;
+    sFingerHold_t* data        = &sFingerHold_data;
+    return data;
+}
+
+void process_hold_down(touch_event_t* event) {
+    start         = event;
+    updated_state = RECOGNIZER_STATE_START;
+}
+
+void process_hold_move(touch_event_t* event) {
+    if (prev) {  // if prev's been initiated
+        if (event->position_x - prev->timestamp > MOVE_RADIUS || event->position_y - prev->position_y > MOVE_RADIUS) {
+            updated_state = RECOGNIZER_STATE_FAILED;
+        }
+    } else if (event->position_x - start->position_x > MOVE_RADIUS ||
+               event->position_y - start->position_y > MOVE_RADIUS) {
+        updated_state = RECOGNIZER_STATE_FAILED;
+    } else {
+        updated_state = RECOGNIZER_STATE_POSSIBLE;
+    }
+}
+
+void process_hold_up(touch_event_t* event) {
+    if (event->position_x - start->position_x > MOVE_RADIUS || event->position_y - start->position_y > MOVE_RADIUS) {
+        updated_state = RECOGNIZER_STATE_FAILED;
+    } else if (event->timestamp - start->timestamp < TIME_DIFF) {
+        updated_state = RECOGNIZER_STATE_FAILED;
+    } else {
+        updated_state = RECOGNIZER_STATE_COMPLETED;
+    }
 }
 
 sFingerHold_t* get_sFingerHold() {

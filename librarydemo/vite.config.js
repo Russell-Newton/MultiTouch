9f113764566fce44ea7417b1ed46ee9068a7b50a
Russell Newton <russell.newton01@gmail.com>
2022-10-31 17:36:42 -0400

    Lint fixes

--- librarydemo/vite.config.js
+++ librarydemo/vite.config.js
@@ -1,28 +1,19 @@
-import path from "path"
-import glob from "glob"
-
 import vue from "@vitejs/plugin-vue"
+import glob from "glob"
 import {fileURLToPath, URL} from "node:url"
+import path from "path"
 import {defineConfig} from "vite"
 import wasm from "vite-plugin-wasm"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, "src"),
-  publicDir: path.join(__dirname, "public"),
-  plugins : [
-    vue(),
-    wasm()
-  ],
-  build: {
-    outDir: path.join(__dirname, "dist"),
-    rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, "src/**/", "*.html"))
-    }   
+  root : path.join(__dirname, "src"),
+  publicDir : path.join(__dirname, "public"),
+  plugins : [ vue(), wasm() ],
+  build : {
+    outDir : path.join(__dirname, "dist"),
+    rollupOptions :
+        {input : glob.sync(path.resolve(__dirname, "src/**/", "*.html"))}
   },
-  resolve : {
-    alias : {
-      '@' : fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  resolve : {alias : {'@' : fileURLToPath(new URL('./src', import.meta.url))}}
 })

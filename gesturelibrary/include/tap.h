292f148cb0067794522fa223fd05c71f7b80d15e
jrdike <jackson.r.dike@gmail.com>
2022-10-31 00:28:35 -0400

    #64 - Implemented Tap in line with stroke

---
+++ gesturelibrary/include/tap.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "gesturelib.h"
+#include "gestureparams.h"
+#include "recognizer.h"
+
+/// @brief Data structure for tap data
+typedef struct tap {
+    state_t state;
+
+    // intial position and time
+    float x0;
+    float y0;
+    float t0;
+
+    // current position
+    float x;
+    float y;
+    float t;
+} tap_t;
+
+/// @brief Recognize tap gesture
+/// @param event touch event to recognize
+gesture_event_t* recognize_tap(touch_event_t* event);
+
+/// @brief Access tap data array of size MAX_TOUCHES
+/// @return Address of first element of tap data array of size MAX_TOUCHES
+sFingerTap_t* get_tap();

6f2e7a04de177f3e9a2c6e6e2584b551332746be
deborahsrcho <deborahsrcho@gmail.com>
2022-11-03 21:49:32 -0400

    Changed layout
    - changed layout of demo page
    - added home button to demo page
    - added buttons to change text displayed on screen

--- librarydemo/src/demo/Demo.vue
+++ librarydemo/src/demo/Demo.vue
@@ -5,12 +5,52 @@ import {test, testStruct} from "./assets/api_wrapper";
 </script>
 
 <template>
-  <header>
-    <MDBBtn @click="test" color="light" size="sm" rounded>Square 3 in C! (Check console output)</MDBBtn>
-    <MDBBtn @click="testStruct" color="light" size="sm" rounded>C Struct test</MDBBtn>
-  </header>
+  <div class="container">
+    <div class="row">
+      <div class="col-md">
+        <header>
+          <a href="../index.html">
+            <MDBBtn color="light" size="sm" rounded>Home</MDBBtn>
+          </a>
+          <MDBBtn @click="test" color="light" size="sm" rounded>Square 3 in C! (Check console output)</MDBBtn>
+          <MDBBtn @click="testStruct" color="light" size="sm" rounded>C Struct test</MDBBtn>
+        </header>
+      </div>
+    <div class="col-md">
+      <main>
+        <GestureCanvas width="500" height="500"/>
+      </main>
+    </div>
+    <div class="col-md">
+      Click to Change Text:
+      <MDBBtn @click="setText('Text A')" color="light" size="sm" rounded>A</MDBBtn>
+      <MDBBtn @click="setText('Text B')" color="light" size="sm" rounded>B</MDBBtn>
+      <h3 >
+        {{ myText }}
+      </h3>
+    </div>
+  </div>
+</div>
   
-  <main>
-    <GestureCanvas width="500" height="500"/>
-  </main>
+  
+
+  
+  
+  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      myText: "Hello World!"
+    }
+  },
+  methods: {
+    setText(newText) {
+      this.myText = newText;
+    }
+  }
+}
+</script>
+

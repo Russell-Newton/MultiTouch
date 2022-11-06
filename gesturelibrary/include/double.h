54c81306a21bb4a3975b9c152f6433fae1c05d6b
Iftekherul Karim <karim@DESKTOP-56DHO31.localdomain>
2022-11-06 14:35:24 -0500

    updated double header

---
+++ gesturelibrary/include/double.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "gesturelib.h"
+#include "math.h"
+#include "recognizer.h"
+#include "stroke.h"
+#include "tap.h"
+
+/// @brief holds the state, last location, and timestamp of a double tap instance
+typedef struct double_tap {
+    state_t state;
+    int x;
+    int y;
+    int t;
+} double_tap_t;  // single finger double tap data
+
+/// @brief tuple containing the x and y coordinates of the double_tap event
+typedef struct coords {
+    int x;
+    int y;
+} coords_t;
+
+/// @brief reads the last tap and stroke data and determines whethers this new stroke is part of a double_tap event
+/// @param event touch event
+gesture_event_t* recognize_double_tap(touch_event_t* event);
+
+/// @brief returns an array of the ongoing double_touch data for each finger
+/// @return
+double_tap_t* get_double_tap();

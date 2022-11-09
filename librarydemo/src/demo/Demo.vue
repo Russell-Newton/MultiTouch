fceee05ec103d71e08887bcd7826754ece5e7d1d
deborahsrcho <deborahsrcho@gmail.com>
2022-11-09 13:13:28 -0500

    Console output now prints to page

--- librarydemo/src/demo/Demo.vue
+++ librarydemo/src/demo/Demo.vue
@@ -12,18 +12,19 @@ import {test, testStruct} from "./assets/api_wrapper";
           <a href="../index.html">
             <MDBBtn color="light" size="sm" rounded>Home</MDBBtn>
           </a>
+          <br />
           <MDBBtn @click="test" color="light" size="sm" rounded>Square 3 in C! (Check console output)</MDBBtn>
           <MDBBtn @click="testStruct" color="light" size="sm" rounded>C Struct test</MDBBtn>
         </header>
       </div>
     <div class="col-md">
       <main>
-        <GestureCanvas width="500" height="500"/>
+        <GestureCanvas :width="500" :height="500" :setParentText="setText"/>
       </main>
     </div>
     <div class="col-md">
       Click to Change Text:
-      <MDBBtn @click="setTextSquare3()" color="light" size="sm" rounded>Square 3 in C! (Check console output)</MDBBtn>
+      <MDBBtn @click="setTextSquare3()" color="light" size="sm" rounded>Square 3 in C!</MDBBtn>
       <MDBBtn @click="setText('Gesture A')" color="light" size="sm" rounded>A</MDBBtn>
       <MDBBtn @click="setText('Gesture B')" color="light" size="sm" rounded>B</MDBBtn>
       <h3 >

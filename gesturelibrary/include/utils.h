02206abfa2661834272571e3695d873be155029d
Iftekherul Karim <karim@DESKTOP-56DHO31.localdomain>
2022-11-22 19:55:06 -0500

    removed print statements, adjusted tap indexing in recognizer, and converted to euc dist

--- gesturelibrary/include/utils.h
+++ gesturelibrary/include/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
-#define SQUARE(x)          ((x) * (x))
-#define SQUARE_SUM(x, y)   (SQUARE(x) + SQUARE(y))
-#define SQUARED_DIST(a, b) (SQUARE_SUM((a)->x - (b)->x, (a)->y - (b)->y))
+#define SQUARE(x)            ((x) * (x))
+#define SQUARE_SUM(x, y)     (SQUARE(x) + SQUARE(y))
+#define SQUARED_DIST(a, b)   (SQUARE_SUM((a)->x - (b)->x, (a)->y - (b)->y))
+#define SQUARED_DIST_0(a, b) (SQUARE_SUM((a)->x - (b).x0, (a)->y - (b).y0))
--- gesturelibrary/src/double.c
+++ gesturelibrary/src/double.c
@@ -3,7 +3,7 @@
 #include "gestureparams.h"
 #include "utils.h"
 
-#include <stdio.h>
+// #include <stdio.h>
 
 double_tap_t double_tap_d[MAX_TOUCHES];
 void (*on_double_tap)(const double_tap_t*) = 0;
@@ -40,12 +40,14 @@ void recognize_double_tap(const touch_event_t* event) {
 
     const tap_t* taps = get_tap();
 
-    for (int i = 0; i < MAX_TOUCHES; i++) {
-        if ((taps + i)->t == event->t && (taps + i)->x == event->x &&
-            (taps + i)->y == event->y) {  // make sure we're not looking at an old completed tap
-            update_double_taps(taps + i, event);  // we don't need event, but it was for the sake of security
-        }
-    }
+    // for (int i = 0; i < MAX_TOUCHES; i++) {
+    //     if ((taps + i)->t == event->t && (taps + i)->x == event->x &&
+    //         (taps + i)->y == event->y) {  // make sure we're not looking at an old completed tap
+    //         update_double_taps(taps + i, event);  // we don't need event, but it was for the sake of security
+    //     }
+    // }
+
+    update_double_taps(taps + event->group, event);
 }
 
 int set_on_double_tap(void (*listener)(const double_tap_t*)) {
@@ -65,8 +67,9 @@ static void update_double_taps(const tap_t* tap, const touch_event_t* event) {
     int group_index    = -1;  // double tap with the closest time the completed tap, will group with this tap
     // int group_number;
 
-    float x_diff;
-    float y_diff;
+    // float x_diff;
+    // float y_diff;
+    float pos_diff;
     float time_diff;
 
     // printf("printing tap_state: %d\n", tap->state);
@@ -86,15 +89,16 @@ static void update_double_taps(const tap_t* tap, const touch_event_t* event) {
 
                 // checking tap position to double tap position
 
-                x_diff    = (event->x - d_tap.x0) < 0 ? (event->x - d_tap.x0) * -1 : (event->x - d_tap.x0);
-                y_diff    = (event->y - d_tap.y0) < 0 ? (event->y - d_tap.y0) * -1 : (event->y - d_tap.y0);
-                time_diff = (event->t - d_tap.t0) < 0 ? (event->t - d_tap.t0) * -1 : (event->t - d_tap.t0);
+                pos_diff = SQUARED_DIST_0(event, d_tap);
+                // x_diff    = (event->x - d_tap.x0) < 0 ? (event->x - d_tap.x0) * -1 : (event->x - d_tap.x0);
+                // y_diff    = (event->y - d_tap.y0) < 0 ? (event->y - d_tap.y0) * -1 : (event->y - d_tap.y0);
+                time_diff = event->t - d_tap.t0;
 
                 // printf("Printing possible case: x = %f\n", d_tap.x0);
                 // printf("Printing tap data: x = %f\n", tap->x);
                 // printf("Printing distances: x_diff = %f, y_diff = %f, time_diff = %f\n", x_diff, y_diff, time_diff);
 
-                if (x_diff < DOUBLE_MAX_X && y_diff < DOUBLE_MAX_Y) {
+                if (pos_diff < DOUBLE_DIST) {
                     if (time_diff < DOUBLE_TIME_DIFF) {
                         // found the corresponding d_tap
                         double_tap_d[i].state = RECOGNIZER_STATE_COMPLETED;
@@ -145,13 +149,13 @@ static void update_double_taps(const tap_t* tap, const touch_event_t* event) {
             }
         }
 
-        for (int j = 0; j < MAX_TOUCHES; j++) {
-            printf("Printing state: %d, x: %f, x0: %f, group: %d\n",
-                   double_tap_d[j].state,
-                   double_tap_d[j].x,
-                   double_tap_d[j].x0,
-                   double_tap_d[j].group);
-        }
+        // for (int j = 0; j < MAX_TOUCHES; j++) {
+        //     printf("Printing state: %d, x: %f, x0: %f, group: %d\n",
+        //            double_tap_d[j].state,
+        //            double_tap_d[j].x,
+        //            double_tap_d[j].x0,
+        //            double_tap_d[j].group);
+        // }
     }
 }
 
@@ -169,87 +173,6 @@ static void update_data0_fields(int index, int group_index, const touch_event_t*
         on_double_tap(&double_tap_d[index]);
     }
 }
-
-// static void update_double_taps(double_tap_t* double_tap, stroke_t* stroke, tap_t* tap, touch_event_t* current_event)
-// {
-//     double_tap->x = stroke->x;
-//     double_tap->y = stroke->y;
-//     double_tap->t = stroke->t;
-
-//     // pass in touch_event and compare the event's x and y to the current stroke we're looking at; if not equal,
-//     we're not currently looking at this stroke
-
-//     printf("printing double_tap: %f, %f, %f\n", double_tap->x, double_tap->y, double_tap->t);
-//     printf("printing stroke: %f, %f, %f, %d\n", stroke->x, stroke->y, stroke->t, stroke->uid);
-//     printf("printing tap: %f, %f, %f\n", tap->x, tap->y, tap->t);
-
-//     // for (int i = 0; i < 0; i++) { //dummy code to not give "variable not used" compilation error
-//     //     int num_taps = m_stroke->size;
-//     //     if (num_taps == 3) {
-//     //         break;
-//     //     }
-//     // }
-
-//     switch (double_tap->state) {
-//     case RECOGNIZER_STATE_IN_PROGRESS:
-//         // look at the timestamp of the tap and the location
-//         // if too far, move on to the next one
-//         // if timestamp is too far, set this double to failed
-
-//         if (tap->state == RECOGNIZER_STATE_COMPLETED &&
-//             stroke->x == current_event->x &&
-//             stroke->y == current_event->y) {
-//             // after completing one double_tap, sift through the double_tap array and look for any other completed
-//             ones of an acceptable distance away and with the same timestamp or a fraction of a unit off (~0.0001)
-//             // upon finding a valid one, group the two together by incrementing all of their num_touches and grouping
-//             them (new "group" field will be needed) together (we need a num_touches and a group, or we just do group
-//             and when interpreting the data we look for how many in the group)
-
-//             float x_dist = tap->x - double_tap->x0;
-//             printf("printing tap_x and double_tap_x: %f %f %f %f %f %f\n", tap->x, double_tap->x0, double_tap->x,
-//             stroke->x, tap->x0, stroke->x0); //mstroke gives null float y_dist = tap->y - double_tap->y0;
-//             printf("printing tap_y and double_tap_y: %f, %f %f %f %f %f\n", tap->y, double_tap->y0, double_tap->y,
-//             stroke->y, tap->y0, stroke->y0);
-
-//             printf("printing x_dist and y_dist: %f, %f\n", x_dist, y_dist);
-//             //Saved the first tap's info in x0, y0, and t0. Actually, I don't think we need this as we can just use
-//             tap's information. UPDATE if (tap->t - double_tap->t0 > DOUBLE_DIFF) {  // if t_diff too big, this dTap
-//             has failed
-//                 double_tap->state = RECOGNIZER_STATE_FAILED;
-//             } else if (x_dist <= DIST_X && y_dist <= DIST_Y) {
-//                 double_tap->state = RECOGNIZER_STATE_COMPLETED;
-//                 //look for other completed
-//                 for (size_t i = 0; i < MAX_TOUCHES; i++) {
-//                     if (double_tap_d[i].state == RECOGNIZER_STATE_COMPLETED) {
-//                         //TODO: grouping logic
-//                     }
-//                 }
-//             }
-//         }
-//         if (on_double_tap) {
-//             on_double_tap(double_tap);
-//         }
-//         break;
-//     case RECOGNIZER_STATE_FAILED:
-//     case RECOGNIZER_STATE_NULL:
-//     case RECOGNIZER_STATE_COMPLETED:
-//         if (tap->state == RECOGNIZER_STATE_COMPLETED &&
-//             stroke->x == current_event->x &&
-//             stroke->y == current_event->y) {
-//             double_tap->state = RECOGNIZER_STATE_IN_PROGRESS;
-//             //beginning of double tap, initialize initial position and time
-//             double_tap->x0 = stroke->x;
-//             double_tap->y0 = stroke->y;
-//             double_tap->t0 = stroke->t;
-//         }
-//         if (on_double_tap) {
-//             on_double_tap(double_tap);
-//         }
-//         break;
-//     default:
-//         return;
-//     }
-// }
 
 const double_tap_t* get_double_tap() {
     return double_tap_d;
